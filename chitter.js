c8564acd89858c992e851a45427b5f4f58408476
David Wickes <dave.wickes@gmail.com>
2014-09-28 11:33:13 +0100

    Adds Jade and changes start script file to start.js

--- chitter.js
+++ chitter.js
@@ -2,9 +2,10 @@ var app = require('express')()
 var server = require('http').Server(app)
 
 app.set('view engine', 'jade')
+app.set('views', __dirname + '/views')
 
 app.get('/', function (request, response) {
-    response.send("<h1>Chitter</h1>")
+    response.render("index")
 });
 
 module.exports = server
